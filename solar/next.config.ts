d37c80fcf4ef8278e0ad81b22a52ca270c106d89
ducbinh <mac@ducbinhs-MacBook-Pro.local>
2025-07-20 00:42:46 +0700

    Update

--- solar/next.config.ts
+++ solar/next.config.ts
@@ -1,8 +1,33 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**/f_avif/**'
+      }
+    ],
+    formats: ['image/avif'],
+    minimumCacheTTL: 86400, // 24 hours cache
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  compress: true,
   output: 'export',
-  distDir: 'out',
+  distDir: 'out'
 };
 
 export default nextConfig;
+
+
+
