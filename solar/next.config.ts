3f1f9542afcbd36524e2fbb5c95007b7e410435a
ducbinh <mac@ducbinhs-MacBook-Pro.local>
2025-07-20 17:43:31 +0700

    Final Product

--- solar/next.config.ts
+++ solar/next.config.ts
@@ -23,8 +23,8 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   compress: true,
-  //output: 'export',
-  //distDir: 'out'
+  output: 'export',
+  distDir: 'out'
 };
 
 export default nextConfig;
